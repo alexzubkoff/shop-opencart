4e44a93aa809c121ec76e025f246f63a23731477
PM <alexzubkoff123@gmail.com>
2018-02-02 17:40:27 +0200

    opencart2.3.0.2rus without modules

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://axs.dev/admin/');
+define('HTTP_CATALOG', 'http://axs.dev/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://axs.dev/admin/');
+define('HTTPS_CATALOG', 'http://axs.dev/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/OSPanel/domains/axs.dev/admin/');
+define('DIR_SYSTEM', 'C:/OSPanel/domains/axs.dev/system/');
+define('DIR_IMAGE', 'C:/OSPanel/domains/axs.dev/image/');
+define('DIR_LANGUAGE', 'C:/OSPanel/domains/axs.dev/admin/language/');
+define('DIR_TEMPLATE', 'C:/OSPanel/domains/axs.dev/admin/view/template/');
+define('DIR_CONFIG', 'C:/OSPanel/domains/axs.dev/system/config/');
+define('DIR_CACHE', 'C:/OSPanel/domains/axs.dev/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/OSPanel/domains/axs.dev/system/storage/download/');
+define('DIR_LOGS', 'C:/OSPanel/domains/axs.dev/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/OSPanel/domains/axs.dev/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/OSPanel/domains/axs.dev/system/storage/upload/');
+define('DIR_CATALOG', 'C:/OSPanel/domains/axs.dev/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opencart_axshare');
+define('DB_PORT', '3306');
+define('DB_PREFIX', '');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://axs.dev/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://axs.dev/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/OSPanel/domains/axs.dev/catalog/');
+define('DIR_SYSTEM', 'C:/OSPanel/domains/axs.dev/system/');
+define('DIR_IMAGE', 'C:/OSPanel/domains/axs.dev/image/');
+define('DIR_LANGUAGE', 'C:/OSPanel/domains/axs.dev/catalog/language/');
+define('DIR_TEMPLATE', 'C:/OSPanel/domains/axs.dev/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/OSPanel/domains/axs.dev/system/config/');
+define('DIR_CACHE', 'C:/OSPanel/domains/axs.dev/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/OSPanel/domains/axs.dev/system/storage/download/');
+define('DIR_LOGS', 'C:/OSPanel/domains/axs.dev/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/OSPanel/domains/axs.dev/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/OSPanel/domains/axs.dev/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opencart_axshare');
+define('DB_PORT', '3306');
+define('DB_PREFIX', '');
