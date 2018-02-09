495fa89006f1c8dd1e69d6aed4bc126058fee293
PM <alexzubkoff123@gmail.com>
2018-02-09 10:25:22 +0200

    modified ControllerInformationPhone for sending recall oreder by e-mail

--- catalog/controller/information/phone.php
+++ catalog/controller/information/phone.php
@@ -15,10 +15,7 @@ class ControllerInformationPhone extends Controller {
         $this->document->setTitle($this->language->get('heading_title'));
 
         if (($this->request->server['REQUEST_METHOD'] == 'POST')) {
-
-            var_dump($this->request->post);
-            exit();
-            /*$mail = new Mail();
+            $mail = new Mail();
             $mail->protocol = $this->config->get('config_mail_protocol');
             $mail->parameter = $this->config->get('config_mail_parameter');
             $mail->smtp_hostname = $this->config->get('config_mail_smtp_hostname');
@@ -28,19 +25,13 @@ class ControllerInformationPhone extends Controller {
             $mail->smtp_timeout = $this->config->get('config_mail_smtp_timeout');
 
             $mail->setTo($this->config->get('config_email'));
+            $mail->setFrom($this->config->get('config_email'));
             //$mail->setFrom($this->request->post['email']);
             $mail->setSender(html_entity_decode($this->request->post['name'], ENT_QUOTES, 'UTF-8'));
-            $mail->setSubject(html_entity_decode(sprintf($this->request->post['phone '], $this->request->post['name']), ENT_QUOTES, 'UTF-8'));
-
-            $mail->setText($this->request->post['name']);
-            $mail->setText($this->request->post['phone']);
+            $mail->setSubject(html_entity_decode(sprintf($this->request->post['phone'], $this->request->post['name']), ENT_QUOTES, 'UTF-8'));
             $mail->setText($this->request->post['subject']);
-
             $mail->send();
-
-            $this->response->redirect($this->url->link('information/contact/success'));*/
-
-
+            $this->response->redirect($this->url->link('information/contact/success'));
         }
     }
 }
