2a519b2438992cdb4e4b1f0a5f77b797b2416d58
PM <alexzubkoff123@gmail.com>
2018-02-12 16:58:10 +0200

    modified home.php and home.tpl for about us information

--- catalog/controller/common/home.php
+++ catalog/controller/common/home.php
@@ -16,6 +16,16 @@ class ControllerCommonHome extends Controller {
 		$data['footer'] = $this->load->controller('common/footer');
 		$data['header'] = $this->load->controller('common/header');
 
+		//$data['aboutus'] = $this->load->controller('information/information');
+
+        $this->load->language('information/information');
+
+        $this->load->model('catalog/information');
+
+        $data['aboutustitle'] = html_entity_decode($this->model_catalog_information->getInformation(4)['title'], ENT_QUOTES, 'UTF-8') . "\n";
+        $data['aboutustext']= html_entity_decode($this->model_catalog_information->getInformation(4)['description'], ENT_QUOTES, 'UTF-8') . "\n";
+        $data['aboutustext'] = substr($data['aboutustext'],0,2000).'...';
+
 		$this->response->setOutput($this->load->view('common/home', $data));
 	}
 }
--- catalog/view/theme/default/template/common/home.tpl
+++ catalog/view/theme/default/template/common/home.tpl
@@ -10,5 +10,10 @@
     <?php } ?>
     <div id="content" class="<?php echo $class; ?>"><?php echo $content_top; ?><?php echo $content_bottom; ?></div>
     <?php echo $column_right; ?></div>
+  <div class="row">
+    <h1><?php echo $aboutustitle; ?></h1>
+    <p><?php echo $aboutustext; ?></p>
+    <a href="./index.php?route=information/information&information_id=4" type="button" class="btn btn-default">Подробнее о нас</a>
+  </div>
 </div>
 <?php echo $footer; ?>
