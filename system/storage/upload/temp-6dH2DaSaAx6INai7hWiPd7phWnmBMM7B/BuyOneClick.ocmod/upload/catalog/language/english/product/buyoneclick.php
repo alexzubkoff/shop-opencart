3ce5ac2982ad60cdcba44eee6167a9363fb0b044
PM <alexzubkoff123@gmail.com>
2018-02-16 11:02:29 +0200

    add fast order click module

---
+++ system/storage/upload/temp-6dH2DaSaAx6INai7hWiPd7phWnmBMM7B/BuyOneClick.ocmod/upload/catalog/language/english/product/buyoneclick.php
@@ -0,0 +1,25 @@
+<?php
+$_['text_buyoneclick_button'] = 'Order in one click';
+
+$_['text_buyoneclick_phone'] = 'Phone number';
+$_['text_buyoneclick_mail'] = 'E-mail';
+$_['text_buyoneclick_comment'] = 'Comment';
+
+$_['text_buyoneclick_input_phone_placeholder'] = 'Your phone number';
+$_['text_buyoneclick_success_title'] = 'Thank you for shopping in our store';
+
+$_['text_buyoneclick_mail_subject'] = 'Quick Order on site';
+$_['text_buyoneclick_mail_msg_data'] = 'When ordering, the following data were';
+$_['text_buyoneclick_mail_msg_order'] = 'Order the following products';
+$_['text_buyoneclick_mail_msg_price'] = 'On the total cost of';
+
+$_['text_buy_one_click']                      = 'Order in 1 click';
+$_['text_buy_one_click_want']                 = 'Want to buy and not want to do more? Set your phone and hit enter, Manager will contact with you soon!';
+$_['text_check_telephone_number']             = 'Check phone number';
+
+
+$_['text_loading']  = 'PLease wait';
+
+$_['text_wrong_number'] = 'Wrong phone number. Please fill correct number';
+$_['text_order_success'] = "Your order is in progress. Soon we will contact with you for more details abouth your order. Order id is ";
+
